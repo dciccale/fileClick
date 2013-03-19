f0c10fa38b8331312273d2045ed082b0094fe00c
Denis Ciccale <dciccale@gmail.com>
2013-03-19 17:36:27 +0100

    update code style

--- js/jquery.fileClick.js
+++ js/jquery.fileClick.js
@@ -1,202 +1,174 @@
 /*!
- * jQuery fileClick Plugin v1.4
- * ----------------------------------------------------------
- * Author: Denis Ciccale (dciccale@gmail.com)
- *
- * Dual licensed under the MIT and GPL licenses:
- *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
+ * jQuery fileClick Plugin 1.5
+ * Copyright (c) 2013 Denis Ciccale (@tdecs)
+ * Released under MIT license (https://raw.github.com/dciccale/placeholder-enhanced/master/LICENSE.txt)
  */
-(function($) {
-	$.fn.fileClick = function(options) {
-			
-		// default options
-		var o = $.extend({}, $.fn.fileClick.defaults, options);
-		
-		return this.each(function(i) {
-			var container = $(this),
-				link = container.find(".jAttachDocument"),
-				inputContainer = container.find(".jFileInputContainer"),
-				NoAttachments = container.find(".jNoAttachments"),
-				FilesList = container.find(".jUploadList");
-				
-				
-				// seteo el tamaño del contenedor del input file para encajar con el elemento disparador
-				inputContainer.css({ width: link[0].offsetWidth, height: link[0].offsetHeight });
-				
-				
-				/* **************************
-				Guarda la lista de ficheros
-				*************************** */
-				var Files = {
-					FileNames: [],
-					Add: function(fileName) { this.FileNames.push(fileName); },
-					Remove: function (fileIndex) { this.FileNames.splice(fileIndex-1, 1); },
-					Count: function () { return this.FileNames.length; },
-					Names: function () { return this.FileNames; }				
-				};
-				
-				
-			   /* *****************************************
-				Funcion después de seleccionar un fichero
-				***************************************** */
-				function onSelect() {
-					// en opera y en ie agrega todo el path, nos quedamos solo con el nombre del fichero
-					var fileName = $(this).val().match(/[^\/\\]+$/)[0];
+(function ($) {
+  $.fn.fileClick = function (options) {
 
-					// si existe un fichero con el mismo nombre muestro un error
-					if (o.ShowAlertMsg && existFile(fileName)) {
-						alert(o.AlertMsg);
-					}
-					// si no lo agrego a la lista
-					else {
-						addFile(fileName);
-					}
-					
-					// quito la clase de hover del link, porque a veces se queda
-					link.removeClass(o.HoverClass);
-					
-					// creo un nuevo input
-					createInput();
-				}
+    // default options
+    var o = $.extend({}, $.fn.fileClick.defaults, options);
+
+    return this.each(function (i) {
+      var container = $(this),
+        link = container.find('.fc-attach-btn'),
+        inputContainer = container.find('.fc-input-container'),
+        noAttachments = container.find('.fc-no-attachments'),
+        filesList = container.find('.fc-upload-list');
+
+        // set container size to match the fake button size
+        inputContainer.css({ width: link[0].offsetWidth, height: link[0].offsetHeight });
+
+        // files list
+        var Files = {
+          fileNames: [],
+          add: function (fileName) {
+            this.fileNames.push(fileName);
+          },
+          remove: function (fileIndex) {
+            this.fileNames.splice(fileIndex-1, 1);
+          }
+        };
 
 
-				/* ***************************
-				Agrega un fichero a la lista
-				**************************** */
-				function addFile(fileName) {
-					var item, inputClone;
-					
-					// guardo el fichero
-					Files.Add(fileName);
+        // when selecting a file
+        function onSelect() {
+          // opera and ie use a full fake path, strip it
+          var fileName = $(this).val().match(/[^\/\\]+$/)[0];
 
-					// template local
-					item = o.ItemTemplate;
+          // show duplicat file error message
+          if (o.allowDuplicates && existFile(fileName)) {
+            alert(o.alertMsg);
 
-					// reemplazo las variables en el template
-					item = item.replace("{FileName}", fileName);
+          // add the file to the list
+          } else {
+            addFile(fileName);
+          }
 
-					// cojo el input modifico el id para que sea unico y lo oculto
-					inputClone = fileInput.getInstance()
-					.unbind('.fileClick')
-					.attr({
-						"id": o.InputID + i + "_" + Files.Count(),
-						"name": o.InputName + i + "[]"
-					})
-					.css({ top: -3000 });
+          // force removing hover class from the link
+          link.removeClass(o.hoverClass);
 
-					// agrego el input file al item
-					item = $(item).append(inputClone);
-					
-					// guardo la referencia al item para poder ser eliminado mas tarde
-					$.data(item.find('.jRemoveFile')[0], 'item', item);
-					
-					// agrego el item
-					FilesList.append(item);
+          // create a new input
+          createInput();
+        }
 
-					// oculto el item de "no hay documentos adjuntos"
-					if (NoAttachments.is(":visible")) {
-						NoAttachments.hide();
-					}
-				}
+        // adds a file to the list
+        function addFile(fileName) {
+          var item, inputClone;
+
+          // save the file in the list object
+          Files.add(fileName);
+
+          // local template
+          item = o.itemTemplate;
+
+          // replace tmpl vars
+          item = item.replace('{FileName}', fileName);
+
+          // make cloned input id unique and hide it
+          inputClone = fileInput.getInstance()
+            .unbind('.fc')
+            .attr({
+              'id': o.inputID + i + '_' + Files.fileNames.length,
+              'name': o.inputName + i + '[]'
+            })
+            .css({ top: -3000 });
+
+          // agrego el input file al item
+          item = $(item).append(inputClone);
+
+          // guardo la referencia al item para poder ser eliminado mas tarde
+          $.data(item.find('.fc-rm-btn')[0], 'item', item);
+
+          // agrego el item
+          filesList.append(item);
+
+          // oculto el item de 'no hay documentos adjuntos'
+          if (noAttachments.is(':visible')) {
+            noAttachments.hide();
+          }
+        }
+
+        // removes a file from the list
+        function removeFile() {
+          var item = $(this);
+
+          // remove from object
+          Files.remove(item.index());
+
+          // remove from list
+          item.remove();
+
+          // if no files, show a message
+          if (!Files.fileNames.length) {
+            noAttachments.show();
+          }
+        }
+
+        // removes a file from the list
+        container
+        .delegate(o.itemTag, 'removeFile.fc', removeFile)
+        .delegate('.fc-rm-btn', 'click.fc', function (ev) {
+          ev.preventDefault();
+          $.data(this, 'item').trigger('removeFile.fc');
+        });
+
+        // check duplicated file
+        function existFile(fileName) {
+          var fileNames = Files.fileNames,
+            i = fileNames.length,
+            exists = false;
+
+          while (i-- && !exists) {
+            exists = fileNames[i] == fileName;
+          }
+
+          return exists;
+        }
+
+        // instance of actual input
+        var fileInput = {
+          setInstance: function (elem) { this.newInput = elem; },
+          getInstance: function () { return this.newInput; }
+        };
+
+        // creates an input file
+        var createInput = (function () {
+
+          function init() {
+            // create a new input
+            var newInput = $('<input type="file" class="' + o.inputDefaultClass + '" id="' + o.inputID + i + '" name="' + o.inputName + i + '[]" />');
+            newInput.bind('mouseover.fc mouseout.fc',
+              function () {
+                link.toggleClass(o.hoverClass);
+              }
+            )
+            // evento que se dispara luego de seleccionar un fichero
+            .bind('change.fc', onSelect);
+
+            // inserto el nuevo input
+            inputContainer.html(newInput);
+
+            // save instance
+            fileInput.setInstance(newInput);
+          }
+
+          init();
+
+          return init;
+        })()
+    });
+  };
 
 
-				/* ***************************
-				Quita un fichero de la lista
-				**************************** */
-				function removeFile() {
-					var item = $(this);
-					
-					// quito el fichero de la lista guardada
-					Files.Remove(item.index());
-
-					// quito el item de la lista
-					item.remove();
-
-					// si no hay ficheros muestro el item de "no hay documentos adjuntos"
-					if (!Files.Count()) {
-						NoAttachments.show();
-					}
-				}
-
-
-				/* ************************************************
-				Evento y Accion que remueve un fichero de la lista
-				************************************************* */
-				container
-				.delegate(o.ItemTag, 'removeFile.fileClick', removeFile)
-				.delegate('.jRemoveFile', 'click.fileClick', function (e) {
-					e.preventDefault();
-					$.data(this, 'item').trigger('removeFile.fileClick');
-				});
-
-
-				/* *************************************************
-				Verifica que no haya un fichero con el mismo nombre
-				************************************************** */
-				function existFile(fileName) {
-					var FileNames = Files.Names(), i = Files.Count(), exists = false;
-					
-					while (i-- && !exists) {
-						exists = FileNames[i] == fileName;
-					}
-					
-					return exists;
-				}
-
-				
-				/* **********************
-				Instancia de cada input
-				*********************** */
-				var fileInput = {
-					setInstance: function (elem) { this.newInput = elem; },
-					getInstance: function () { return this.newInput; }
-				};
-				
-				
-				/* *****************
-				Crea el input file
-				****************** */
-				var createInput = (function () {
-				
-					function init() {
-						// creo el nuevo input
-						var newInput = $("<input type='file' class='" + o.InputDefaultClass + "' id='" + o.InputID + i + "' name='" + o.InputName + i + "[]' />");
-
-						// guardo la instancia del input
-						fileInput.setInstance(newInput);
-
-						// hago un bind de los eventos
-						newInput
-						// clase de hover para el link
-						.bind('mouseover.fileClick mouseout.fileClick',
-							function () {
-								link.toggleClass(o.HoverClass);
-							}
-						)
-						// evento que se dispara luego de seleccionar un fichero
-						.bind('change.fileClick', onSelect);
-
-						// inserto el nuevo input
-						inputContainer.html(newInput);
-					}
-					
-					init();
-					
-					return init;
-				})()
-		});
-	};
-	
-	
-	$.fn.fileClick.defaults = {
-		HoverClass: 'jUploadButtonHover',
-		ItemTemplate: '<li><a href="#" class="jRemoveFile">X</a>{FileName}</li>',
-		InputID: 'InputID',
-		InputName: 'Files',
-		ItemTag: 'li',
-		InputDefaultClass: 'jInputFile',
-		ShowAlertMsg: true,
-		AlertMsg: 'Ya existe un fichero con ese nombre'
-	}
+  $.fn.fileClick.defaults = {
+    hoverClass: 'fc-upload-btn-hover',
+    itemTemplate: '<li><a href="#" class="fc-rm-btn">X</a>{FileName}</li>',
+    inputID: 'inputID',
+    inputName: 'files',
+    itemTag: 'li',
+    inputDefaultClass: 'fc-file-input',
+    allowDuplicates: false,
+    duplicatesMessage: 'File already added'
+  }
 })(jQuery);
